513342c1968f9bc7ab5a7e764b61670fbd3df234
Madd Eye <maddeye@arcor.de>
2021-02-09 11:26:14 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@
 if [ "$(docker images -q tsdebug)" = "" ]; then
   docker build -t tsdebug - < ./container/Dockerfile
 fi
-docker run --label "TSDebugContainer" --rm -v "$(pwd)":/app tsdebug:latest sh -c "npm i && npm run start"
+docker run --label "TSDebugContainer" --rm -v "$(pwd)":/app tsdebug:latest sh -c "npm i && npm run $1"
